36aa9f65ed187612bde5c3d72d2deea593fe67bb
Caio Fernando Soares Deambrosio <caio.deambrosio@gmail.com>
2019-10-19 16:07:29 -0300

    creating modules

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php include './shared/head.html' ?>
+
+<?php include './shared/sidebar.html' ?>
+
+<?php include './shared/body.html' ?>
+
+</html>
+<?php include './shared/scripts.html' ?>
